24fb8607219fc5f021fcafe8ec93af96d01f3ba7
Kota Tsuyuzaki <tsuyuzaki.kota@lab.ntt.co.jp>
2016-12-22 02:11:58 -0800

    Add storlet plug-in

--- IPython/core/magics/osm.py
+++ IPython/core/magics/osm.py
@@ -790,19 +790,9 @@ class OSMagics(Magics):
         with io.open(filename, mode, encoding='utf-8') as f:
             f.write(cell)
 
-    @magic_arguments.magic_arguments()
-    @magic_arguments.argument(
-        'container_obj', type=unicode_type,
-        help='container/object path to upload'
-    )
-    @cell_magic
-    def uploadfile(self, line, cell):
-        """Upload fthe contents of the cell to OpenStack Swift.
+    def _uploadfile(self, container, obj, content, headers):
+        """Submethod to upload content to Swift.
         """
-        args = magic_arguments.parse_argstring(self.uploadfile, line)
-        container_obj = os.path.expanduser(args.container_obj)
-        container, obj = container_obj.split('/', 1)
-
         # host, username, key
         try:
             auth_url = os.environ['ST_AUTH']
@@ -815,5 +805,47 @@ class OSMagics(Magics):
         conn = Connection(auth_url, auth_user, auth_password)
 
         conn.put_object(
-            container, obj, cell,
-            headers={"Content-Type": "application/python"})
+            container, obj, content,
+            headers=headers)
+
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument(
+        'container_obj', type=unicode_type,
+        help='container/object path to upload'
+    )
+    @cell_magic
+    def uploadfile(self, line, cell):
+        """Upload fthe contents of the cell to OpenStack Swift.
+        """
+        args = magic_arguments.parse_argstring(self.uploadfile, line)
+        container, obj = args.container_obj.split('/', 1)
+        self._uploadfile(continer, obj, cell,
+                         {'Content-Type': 'application/python'})
+
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument(
+        'module_class', type=unicode_type,
+        help='module and class name to upload'
+    )
+    @magic_arguments.argument(
+        '-c', '--container', type=unicode_type, default='storlet',
+        help='Storlet container name, "storlet" in default'
+    )
+    @magic_arguments.argument(
+        '-d', '--dependencies', type=unicode_type, default='storlet',
+        help='Storlet container name, "storlet" in default'
+    )
+    @cell_magic
+    def uploadstorlet(self, line, cell):
+        args = magic_arguments.parse_argstring(self.uploadstorlet, line)
+        module_path = args.module_class
+        assert module_path.count('.') == 1
+        headers = {
+            'X-Object-Meta-Storlet-Language': 'python',
+            'X-Object-Meta-Storlet-Interface-Version': '1.0',
+            'X-Object-Meta-Storlet-Object-Metadata': 'no',
+            'X-Object-Meta-Storlet-Main': module_path,
+            'Content-Type': 'application/octet-stream',
+        }
+        obj_name = '%s.py' % module_path.split('.')[0]
+        self._uploadfile(args.container, obj_name, cell, headers)
